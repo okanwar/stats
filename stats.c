bfebb862fd39151873a0a700bf1a308c92f3210c
Sat Garcia <elsaturnino@gmail.com>
2016-09-30 14:55:58 -0700

    code cleanup in aisle 3

--- stats.c
+++ stats.c
@@ -12,15 +12,16 @@
 double *getValues(int *size, int *capacity, char *filename);
 
 int main(int argc, char *argv[]) {
-  if(argc != 2) {
-    printf ("usage: ./stats filename\n");
-    exit(1);
-  }
+	if(argc != 2) {
+		printf ("usage: ./stats filename\n");
+		exit(1);
+	}
 
-  // argv[1] contains filename string (pass argv[1] as the
-  // third argument to getValues)
-  printf("A DEBUG MESSAGE: REMOVE THIS!!!! filename %s\n", argv[1]);
+	// argv[1] contains filename string (pass argv[1] as the
+	// third argument to getValues)
+	printf("A DEBUG MESSAGE: REMOVE THIS!!!! filename %s\n", argv[1]);
 
+	return 0;
 }
 
 /** 
